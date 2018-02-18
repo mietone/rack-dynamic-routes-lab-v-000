90e452ec90ea5337bb2f5211b638fc947be04b76
Miwa <paintfx@gmail.com>
2018-02-18 01:06:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,16 @@ class Application
     req = Rack::Reauest.new
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items").last
-      item = @@items.find{|i| i.name == item_name}
+        item_name = req.path.split("/items").last
+        item = @@items.find{|i| i.name == item_name}
+        if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 404
     else
+      resp.write "Route not found"
+      resp.status = 404
     end
   end
 end
