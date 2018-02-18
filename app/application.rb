2e5b58455b783aef91a338ae288452ffba3428b4
Miwa <paintfx@gmail.com>
2018-02-18 01:09:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
         item_name = req.path.split("/items").last
         item = @@items.find{|i| i.name == item_name}
       if item
-        resp.write item.price
+        resp.write item.price.to_s
       else
         resp.write "Item not found"
         resp.status = 404
