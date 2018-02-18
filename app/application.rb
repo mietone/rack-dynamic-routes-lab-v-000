c4315495e27dc65c9c7ee50154a84c3e9bec6bae
Miwa <paintfx@gmail.com>
2018-02-18 01:10:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new
 
     if req.path.match(/items/)
-        item_name = req.path.split("/items").last
+        item_name = req.path.split("/items/").last
         item = @@items.find{|i| i.name == item_name}
       if item
         resp.write item.price.to_s
