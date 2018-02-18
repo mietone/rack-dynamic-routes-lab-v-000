4c47fbbd050f62c06176048a502a27eee6fa4cf1
Miwa <paintfx@gmail.com>
2018-02-18 01:07:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Reauest.new
+    req = Rack::Request.new
 
     if req.path.match(/items/)
         item_name = req.path.split("/items").last
